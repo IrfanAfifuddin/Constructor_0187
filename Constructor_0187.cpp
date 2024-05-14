71ddf684bde53e606e0d61fcdacef4f945c0c3b4
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-05-14 10:42:52 +0700

    membuat include string dan using namespace std

--- Constructor_0187.cpp
+++ Constructor_0187.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
+class Mahasiswa {
 
 public:
     static int nim;
     int id;
     string nama;
+
+    void setID();
+    void printAll();
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
 };
 
-int main()
-{
-    
+int Mahasiswa::nim = 187;
+
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    system("pause");
+
+    return 0;
 }
